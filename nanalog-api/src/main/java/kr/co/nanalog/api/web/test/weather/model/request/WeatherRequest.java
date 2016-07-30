0e63ea9439f7c784644bf358b9a1a5878d7fa7d7
Ga-in Lee <dlrkdls91@naver.com>
2016-07-30 21:01:24 +0900

    [Test-004]날씨 api

---
+++ nanalog-api/src/main/java/kr/co/nanalog/api/web/test/weather/model/request/WeatherRequest.java
@@ -0,0 +1,44 @@
+package kr.co.nanalog.api.web.test.weather.model.request;
+
+/**
+ * Created by Leegain on 2016-07-30.
+ */
+public class WeatherRequest {
+    private String city;
+    private String country;
+    private String village;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getVillage() {
+        return village;
+    }
+
+    public void setVillage(String village) {
+        this.village = village;
+    }
+
+
+    @Override
+    public String toString() {
+        return "WeatherParam{" +
+                "city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", village='" + village +
+                '}';
+    }
+}
