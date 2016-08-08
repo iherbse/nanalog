9fc6016e4f479fa6213871b86770656e5aef67bc
Cheolwon <lcw@lcw.local>
2016-08-08 17:15:06 +0900

    semantic install

--- nanalog-react/containers/App.js
+++ nanalog-react/containers/App.js
@@ -1,28 +1,27 @@
-import React, { Component, PropTypes } from 'react'
-import { connect } from 'react-redux'
-import { browserHistory } from 'react-router'
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
+import {browserHistory} from 'react-router'
 
 
-class App extends Component{
-  constructor(props) {
-    super(props)
-  }
+class App extends Component {
+    constructor(props) {
+        super(props)
+    }
 
-  render(){
-    const { children } = this.props
+    render() {
+        const {children} = this.props
 
-    return(
-    <div>
-    히익!!!!!!! 늦어서 죄송합니당
-    {children}
-    </div>
-    )
-  }
+        return (
+            <button className="ui button">
+                follow
+            </button>
+        )
+    }
 }
 
 App.propTypes = {
-  // Injected by React Redux  // Injected by React Router
-  children: PropTypes.node
+    // Injected by React Redux  // Injected by React Router
+    children: PropTypes.node
 }
 
 export default App;
