bcf12ab586994265d3b4f3bf6ff666b44bda2fc7
Cheolwon <lcw@lcw.local>
2016-08-09 20:56:01 +0900

    semantic-ui install

--- nanalog-react/containers/App.js
+++ nanalog-react/containers/App.js
@@ -1,8 +1,6 @@
 import React, {Component, PropTypes} from 'react'
 import {connect} from 'react-redux'
 import {browserHistory} from 'react-router'
-import Button from 'react-bootstrap/lib/Button';
-import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
 
 class App extends Component {
     constructor(props) {
@@ -13,28 +11,16 @@ class App extends Component {
         const {children} = this.props
 
         return (
-            <ButtonToolbar>
-                {/* Standard button */}
-                <Button>Default</Button>
+            <div className="ui labeled button" tabindex="0">
+                <div className="ui button">
+                    <i className="heart icon"></i>
+                    Like
+                </div>
+                <a className="ui basic label">
+                    2,048
+                </a>
+            </div>
 
-                {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
-                <Button bsStyle="primary">Primary</Button>
-
-                {/* Indicates a successful or positive action */}
-                <Button bsStyle="success">Success</Button>
-
-                {/* Contextual button for informational alert messages */}
-                <Button bsStyle="info">Info</Button>
-
-                {/* Indicates caution should be taken with this action */}
-                <Button bsStyle="warning">Warning</Button>
-
-                {/* Indicates a dangerous or potentially negative action */}
-                <Button bsStyle="danger">Danger</Button>
-
-                {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
-                <Button bsStyle="link">Link</Button>
-            </ButtonToolbar>
         )
     }
 }
--- nanalog-react/webpack.config.js
+++ nanalog-react/webpack.config.js
@@ -1,32 +1,36 @@
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports={
-  devtool:'inline-source-map',
-  entry:[
-    'webpack-hot-middleware/client',
-    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
-    './client/client.js'
-  ],
-  output:{
-    path: require('path').resolve("./dist"),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  plugins:[
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  module:{
-    loaders:[
-      {
-        test:/\.js$/,
-        loader: 'babel-loader',
-        exclude:/node_modules/,
-        query:{
-          presets:['react','es2015']
-        }
-      }
-    ]
-  }
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+        './client/client.js',
+        './semantic/dist/semantic.min.css',
+        './semantic/dist/semantic.min.js',
+    ],
+    output: {
+        path: require('path').resolve("./dist"),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style', '!css!autoprefixer?browsers=last 2 version')
+        }, ]
+    }
 }
