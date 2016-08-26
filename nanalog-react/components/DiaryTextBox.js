4eccb038fee90d75cc2da9335022a253c9cdc0e0
Cheolwon <lcw@lcw.local>
2016-08-27 05:03:10 +0900

    no message

--- nanalog-react/components/DiaryTextBox.js
+++ nanalog-react/components/DiaryTextBox.js
@@ -1,8 +1,6 @@
 import React, {Component, PropTypes} from 'react';
-import { createComponent, createPage ,deletePage} from '../actions/diary'
-import { connect } from 'react-redux'
-
-
+import {createComponent, createPage, deletePage} from '../actions/diary'
+import {connect} from 'react-redux'
 
 function today() {
 
@@ -22,108 +20,97 @@ function today() {
     return year + "." + month + "." + day;
 }
 
-function renderTodayPage(title,content,image,today,todayPage) {
-  if(typeof todayPage !== "undefined"){
-  if(todayPage.length>0){
-  return(
-    <div>
-        <span className="diary_created_date">{today}</span>
-        <img src={require('../images/btn_current_time.svg')} className="btn_current_time"></img>
-        <img src={require('../images/btn_trash.svg')} className="btn_trash"></img>
-        <input type="text" className="diary_title" value={title}></input>
-        <hr className="seperate_title_contents" value = {content}></hr>
-        <div className="btn_diary_edit"></div>
+function renderTodayPage(title, content, image, today, todayPage) {
+    if (typeof todayPage !== "undefined") {
+        if (todayPage.length > 0) {
+            return (
+                <div className="diary-textbox-inner">
+                    <span className="diary_created_date">{today}</span>
+                    <img src={require('../images/btn_current_time.svg')} className="btn_current_time"></img>
+                    <img src={require('../images/btn_trash.svg')} className="btn_trash"></img>
+                    <input type="text" className="diary_title" disabled value={title}></input>
+                    <hr className="seperate_title_contents"></hr>
+                    <textarea className="diary_contents" disabled value={content}></textarea>
+                    <div className="btn_diary_edit">편집</div>
 
-        <div className="ui small modal">
-            <i className="close icon"></i>
-            <div className="header">
-                정말 일기를 삭제하시겠습니까?
-            </div>
+                    <div className="ui small modal">
+                        <div className="header">
+                            정말 일기를 삭제하시겠습니까?
+                        </div>
+                        <div className="actions">
+                            <div className="negative ui button">삭제</div>
+                            <div className="ui button">취소</div>
+                        </div>
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div className="diary-textbox-inner">
+                    <span className="diary_created_date">{today}</span>
+                    <img src={require('../images/btn_current_time.svg')} className="btn_current_time"></img>
+                    <img src={require('../images/btn_trash.svg')} className="btn_trash"></img>
+                    <input type="text" className="diary_title" placeholder="제목을 입력하세요"></input>
+                    <hr className="seperate_title_contents"></hr>
+                    <textarea className="diary_contents" placeholder="어떤 것을 남기고 싶나요?"></textarea>
+                    <div className="btn_diary_edit">편집</div>
 
-            <div className="actions">
-                <div className="negative ui button">삭제</div>
-                <div className="ui button">취소</div>
-            </div>
-        </div>
-    </div>
-  )
-}else{
-  return(
-    <div >
-        <span className="diary_created_date">{today}</span>
-        <img src={require('../images/btn_current_time.svg')} className="btn_current_time"></img>
-        <img src={require('../images/btn_trash.svg')} className="btn_trash"></img>
-        <input type="text" className="diary_title" disabled placeholder="제목을 입력하세요"></input>
-        <hr className="seperate_title_contents"></hr>
+                    <div className="ui small modal">
+                        <div className="header">
+                            정말 일기를 삭제하시겠습니까?
+                        </div>
+                        <div className="actions">
+                            <div className="negative ui button">삭제</div>
+                            <div className="ui button">취소</div>
 
-        <div className="btn_diary_edit"></div>
-
-        <div className="ui small modal">
-            <i className="close icon"></i>
-            <div className="header">
-                정말 일기를 삭제하시겠습니까?
-            </div>
-
-            <div className="actions">
-                <div className="negative ui button">삭제</div>
-                <div className="ui button">취소</div>
-
-            </div>
-        </div>
-    </div>
-  )
-}
-}
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
 }
 
 class DiaryTextBox extends Component {
     render() {
-      const {title,content,image, today, todayPage} = this.props;
-      console.log(todayPage);
-        return(
-          <div className="diary-textbox">
-          {renderTodayPage(title,content,image,today,todayPage)}
-          </div>
+        const {title, content, image, today, todayPage} = this.props;
+        console.log(todayPage);
+        return (
+            <div className="diary-textbox">
+                {renderTodayPage(title, content, image, today, todayPage)}
+            </div>
         )
 
     };
 
 }
 function mapStateToProps(state, ownProps) {
-  console.log("ownProps!!");
-  console.log(ownProps);
-  const {todayPage} = ownProps;
-  if(typeof todayPage !==  "undefined"){
-    var title ;
-    var content;
-    var image;
-    for(var i = 0; i < todayPage.length; i++){
-      if(todayPage[i].type == 1){
-        title = todayPage[i].body;
-      }else if(todayPage[i].type == 2){
-        content = todayPage[i].body;
-      }else if(todayPage[i].type == 3){
-        image = todayPage[i].body;
-      }
+    console.log("ownProps!!");
+    console.log(ownProps);
+    const {todayPage} = ownProps;
+    if (typeof todayPage !== "undefined") {
+        var title;
+        var content;
+        var image;
+        for (var i = 0; i < todayPage.length; i++) {
+            if (todayPage[i].type == 1) {
+                title = todayPage[i].body;
+            } else if (todayPage[i].type == 2) {
+                content = todayPage[i].body;
+            } else if (todayPage[i].type == 3) {
+                image = todayPage[i].body;
+            }
+        }
+        return {title: title, content: content, image: image}
+    } else {
+        return {}
     }
-    return{
-      title : title,
-      content : content,
-      image : image
-    }
-  }else{
-    return {
-
-    }
-  }
 }
 DiaryTextBox.propTypes = {
-  pageId : PropTypes.number,
-  title : PropTypes.string,
-  content: PropTypes.string,
-  image : PropTypes.string,
-  today : PropTypes.string
+    pageId: PropTypes.number,
+    title: PropTypes.string,
+    content: PropTypes.string,
+    image: PropTypes.string,
+    today: PropTypes.string
 }
-export default connect(mapStateToProps,{
-  createComponent,createPage,deletePage
-})(DiaryTextBox);
+export default connect(mapStateToProps, {createComponent, createPage, deletePage})(DiaryTextBox);
--- nanalog-react/components/Indicator.js
+++ nanalog-react/components/Indicator.js
@@ -1,67 +1,73 @@
 import React, {Component, PropTypes} from 'react';
-import { fetchPreviewPage } from '../actions/diary'
-import { connect } from 'react-redux'
+import {fetchPreviewPage} from '../actions/diary'
+import {connect} from 'react-redux'
 import moment from 'moment';
 
-
-function getIndicator(pageList,weeklistOfPageList) {
-  let return_val = [];
-
-  for(var i=0 ; i < 7 ; i++){
-    for(var j=0; j < weeklistOfPageList.length; j++){
-      if(Number(weeklistOfPageList[j]) == i){
-        console.log("i = "+i);
-        console.log("j = "+j);
-        return_val.push( <li><img src={require('../images/indicator_filled.svg')} className= {`indicator_${i}`} ></img></li> )
-        break;
-      }
+function getIndicator(pageList, weeklistOfPageList) {
+    let return_val = [];
+    let j = 0;
+    if (j < weeklistOfPageList.length) {
+        for (var i = 1; i <= 7; i++) {
+            if (weeklistOfPageList[j] == i) {
+                return_val.push(
+                    <li>
+                        <img src={require('../images/indicator_filled.svg')} className={`indicator_${i}`}></img>
+                    </li>
+                )
+                j++;
+            } else {
+                return_val.push(
+                    <li>
+                        <img src={require('../images/indicator_unfilled.svg')} className={`indicator_${i}`}></img>
+                    </li>
+                )
+            }
+        }
     }
-    return_val.push( <li><img src={require('../images/indicator_unfilled.svg')} className= {`indicator_${i}`}></img></li> )
-  }
-  return (
-    <div>
-      {return_val}
-    </div>
-  )
+    return (
+        <div>
+            {return_val}
+        </div>
+    )
 }
 class Indicator extends Component {
 
-  //  {this.state.pageList.map(getIndicator)}
+    //  {this.state.pageList.map(getIndicator)}
 
     render() {
-      console.log("Indicator props");
-      console.log(this.props);
-      const {pageList, weeklistOfPageList} = this.props;
+        console.log("Indicator props");
+        console.log(this.props);
+        const {pageList, weeklistOfPageList} = this.props;
         return (
-              <div className="diary-indicator">
+            <div className="diary-indicator">
                 <ul>
-                  <li><img src={require('../images/indicator_up.svg')} className="indicator_up"></img></li>
-                  {getIndicator(pageList,weeklistOfPageList)}
-                  <li><img src={require('../images/indicator_up.svg')} className="indicator_down"></img></li>
+                    <li>
+                        <img src={require('../images/indicator_up.svg')} className="indicator_up"></img>
+                    </li>
+                    {getIndicator(pageList, weeklistOfPageList)}
+                    <li>
+                        <img src={require('../images/indicator_up.svg')} className="indicator_down"></img>
+                    </li>
                 </ul>
-              </div>
+            </div>
         )
     };
 
 }
 function mapStateToProps(state, ownProps) {
-  const {pageList} = ownProps;
-  if(typeof pageList !==  "undefined"){
-    var weeklistOfPageList = [];
-    for(var i = 0; i < pageList.length; i++){
-      var momentObj = moment(pageList[i].date, 'YYYYMMDD')
-      weeklistOfPageList[i] = momentObj.day();
+    const {pageList} = ownProps;
+    if (typeof pageList !== "undefined") {
+        var weeklistOfPageList = [];
+        for (var i = 0; i < pageList.length; i++) {
+            var momentObj = moment(pageList[i].date, 'YYYYMMDD')
+            weeklistOfPageList[i] = momentObj.day();
+        }
+        return {weeklistOfPageList: weeklistOfPageList}
     }
-    return{
-      weeklistOfPageList : weeklistOfPageList
-    }
-  }
 }
 Indicator.propTypes = {
-  pageList :PropTypes.array.isRequired,
-  weeklistOfPageList : PropTypes.array
+    pageList: PropTypes.array.isRequired,
+    weeklistOfPageList: PropTypes.array
 
 }
-export default connect(mapStateToProps,{
-  fetchPreviewPage
-})(Indicator);
+export default connect(mapStateToProps, {fetchPreviewPage})(Indicator);
